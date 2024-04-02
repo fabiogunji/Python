9255ad7f78b5f780e58a2cef57b5ccab706cbdb2
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-02 14:04:57 -0300

    update

--- Atividade Aula 04 - Condicões Aninhadas/desafio6.py
+++ Atividade Aula 04 - Condicões Aninhadas/desafio6.py
@@ -15,12 +15,42 @@ try:
         jogadaComputador = random.randint(0,2)
     
         jogador = int(input("Entre com um número para jogar: 0 - Pedra |  - Papel | 2 - Tesoura: "))
+        
+        print(dicionario[jogadaComputador])
+        print(dicionario[jogador])
 
-        if (dicionario[jogadaComputador] == dicionario[jogador]):
-            print(f"Você ganhou! - {dicionario[jogadaComputador]}")
+        if dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Papel":
+            print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
             acertou = True
-        else:
+        elif dicionario[jogadaComputador] == "Papel" and dicionario[jogador] == "Pedra":
             print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True 
+             
+        elif dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Tesoura":
+            print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True 
+        elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Pedra":
+            print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")        
+            acertou = True        
+                
+        elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Papel":
+            print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True
+        elif dicionario[jogadaComputador] == "Papel" and dicionario[jogador] == "Tesoura":
+            print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True   
+                
+        elif dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Tesoura":
+            print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True  
+        elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Pedra":
+            print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            acertou = True  
+        
+        elif dicionario[jogadaComputador] == dicionario[jogador]:
+            print(f"Empate! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+                  
+       
 except:
     print("Entre com valores válidos")
 
--- Atividade Aula 04 - Condicões Aninhadas/desafio6_2.py
+++ Atividade Aula 04 - Condicões Aninhadas/desafio6_2.py
@@ -5,3 +5,9 @@ opcoes = ["PEDRA", "PAPEL", "TESOURA"]
 escolhaMaquina = choice(opcoes)
 
 escolhaUsuario = input("Informe a sua escolha: ")
+
+if escolhaMaquina  == escolhaUsuario:
+    print(f"Ambos escolheram {escolhaMaquina}, portanto deu empate")
+    
+elif escolhaMaquina == "PEDRA" and escolhaUsuario == "TESOPURA":
+    print(f"A máquina escolheu {escolhaMaquina}, portanto ela venceu")
