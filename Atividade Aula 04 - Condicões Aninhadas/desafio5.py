a38e435b356dc6f744894f808f6ac9224d4218e5
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-02 16:10:11 -0300

    update

--- Atividade Aula 04 - Condicões Aninhadas/desafio5.py
+++ Atividade Aula 04 - Condicões Aninhadas/desafio5.py
@@ -1,11 +1,8 @@
 #DESAFIO 05
 
 #Refaça o DESAFIO 35 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
-
 #Equilátero: Todos os lados iguais
-
 #Isósceles: Dois lados iguais
-
 #Escaleno: Todos os lados diferentes
 
 reta1 = int(input("Entre com o comprimento da reta 1: ")) #a
@@ -23,9 +20,9 @@ ac = reta1 + reta3
 bc = reta2 + reta3
 
 if (ab > reta3) and (ac > reta2) and (bc > reta1):    
-    if (reta1 == reta2) and ( reta2 == reta3):    
+    if reta1 == reta2 == reta3:    
         print(f"É um triangulo equilátero. {reta1}, {reta2} e {reta3}")    
-    elif (reta1 != reta2) and ( reta2 != reta3):   
+    elif reta1 != reta2 != reta3 != reta1:   
         print(f"É um triangulo escaleno. {reta1}, {reta2} e {reta3}")
     else:
         print(f"É um triangulo isósceles. {reta1}, {reta2} e {reta3}")
