dc6c2df8e3ad82774a5d1567dcf5e8f1f1452864
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-02 14:13:43 -0300

    update

--- Atividade Aula 04 - Condicões Aninhadas/desafio6.py
+++ Atividade Aula 04 - Condicões Aninhadas/desafio6.py
@@ -14,41 +14,56 @@ try:
         
         jogadaComputador = random.randint(0,2)
     
-        jogador = int(input("Entre com um número para jogar: 0 - Pedra |  - Papel | 2 - Tesoura: "))
+        jogador = int(input("Entre com um número para jogar: 0 - Pedra | 1 - Papel | 2 - Tesoura: "))
         
-        print(dicionario[jogadaComputador])
-        print(dicionario[jogador])
-
         if dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Papel":
+            print("_____________________________________________________________________________________________")
             print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True
         elif dicionario[jogadaComputador] == "Papel" and dicionario[jogador] == "Pedra":
+            print("_____________________________________________________________________________________________")
             print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True 
              
         elif dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Tesoura":
+            print("_____________________________________________________________________________________________")
             print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True 
         elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Pedra":
+            print("_____________________________________________________________________________________________")
             print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")        
+            print("_____________________________________________________________________________________________")
             acertou = True        
                 
         elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Papel":
+            print("_____________________________________________________________________________________________")
             print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True
         elif dicionario[jogadaComputador] == "Papel" and dicionario[jogador] == "Tesoura":
+            print("_____________________________________________________________________________________________")
             print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True   
                 
         elif dicionario[jogadaComputador] == "Pedra" and dicionario[jogador] == "Tesoura":
+            print("_____________________________________________________________________________________________")
             print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True  
         elif dicionario[jogadaComputador] == "Tesoura" and dicionario[jogador] == "Pedra":
+            print("_____________________________________________________________________________________________")
             print(f"Você ganhou! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
             acertou = True  
         
         elif dicionario[jogadaComputador] == dicionario[jogador]:
+            print("_____________________________________________________________________________________________")
             print(f"Empate! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+            print("_____________________________________________________________________________________________")
                   
        
 except:
