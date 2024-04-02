853b80a2451255a622b894cb0054b768a63aad55
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-02 05:12:44 -0300

    desafio 6

--- Atividade Aula 04 - Condicões Aninhadas/desafio6.py
+++ Atividade Aula 04 - Condicões Aninhadas/desafio6.py
@@ -3,3 +3,29 @@
 
 #Crie um programa que faça o computador jogar Jokenpô com
 
+import random
+
+dicionario = {0:"Pedra", 1:"Papel", 2:"Tesoura"}
+
+acertou = False
+
+try:
+    while acertou == False:
+        
+        jogadaComputador = random.randint(0,2)
+    
+        jogador = int(input("Entre com um número para jogar: 0 - Pedra |  - Papel | 2 - Tesoura: "))
+
+        if (dicionario[jogadaComputador] == dicionario[jogador]):
+            print(f"Você ganhou! - {dicionario[jogadaComputador]}")
+            acertou = True
+        else:
+            print(f"Você perdeu! - Comnputador: {dicionario[jogadaComputador]} e Jogador: {dicionario[jogador]}")
+except:
+    print("Entre com valores válidos")
+
+
+
+
+
+
