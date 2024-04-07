cb0577134d2507bf513f3e12681e3f76aef69b91
Fabio Jorge Gunji <48600898+fabiogunji@users.noreply.github.com>
2024-04-06 21:17:28 -0300

    Update desafio6.py
    update

--- Atividade Aula 05 - While/desafio6.py
+++ Atividade Aula 05 - While/desafio6.py
@@ -9,48 +9,38 @@
     # C) Qual é o nome do produto mais barato.
     
 
-qtdProduto = totalGasto = 0
-vlrMaior100 = 0.0   
-prdMaiort100 = 100.0 
-vlrCompra = 0.0
-lstdMaisBarato = []
-prdMaisbarato = " "
+vlrMaior100 = qtdProduto = totalGasto = contador = prdMaiort100 =  0
+vlrCompra =  vlrMenorPreco = 0.0    
+nmProdutoMaisBarato = " "
+dictProduto = {}
 
-def validaPreco(vlr1, vlr2,nome,preco):
-    if (vlr1 < vlr2): 
-        numeroMenor = vlr1        
-    else:             
-        numeroMenor = vlr1 
-    
-    return numeroMenor,nome,preco
+def validaPreco(produto, preco, vlrMenorPreco):        
+    if (preco < vlrMenorPreco): 
+        dictProduto.update({produto:preco})       
+        
+        
 
 while True:    
     produto = (input("Digite o nome do produto: ")).upper()
-    preco = float(input("Digite o preço desse produto: "))
-    
+    preco = float(input("Digite o preço desse produto: "))   
+      
+    nmProdutoMaisBarato = produto
+    qtdProduto += 1        
+        
+    if preco > 100.0:
+        vlrMaior100 += 1  
+        vlrCompra += preco  
+    else:
+        vlrCompra += preco        
+               
+    validaPreco(produto, preco, vlrMenorPreco)
+
     continuar = input("Deseja continuar? [S/N]: ").upper()
     
-    if continuar == "S":
-        qtdProduto += 1        
-        
-        if preco > prdMaiort100:
-            vlrMaior100 += 1  
-            vlrCompra += preco  
-        else:
-            vlrCompra += preco             
-        
-        lstdMaisBarato.append(validaPreco(preco, preco,produto,preco))
-            
-        continue
-    elif continuar == "N":    
-        if preco > prdMaiort100:
-            vlrMaior100 += 1  
-            vlrCompra += preco 
-            
+    if continuar == "N":        
         break
-    else:
-        print("Favor entrar con S ou N.")
+
         
-print(f"#### A) Qual é o total gasto na compra: {vlrCompra}. ####")
-print(f"#### B) Quantos produtos custam mais de R$ 100,00: {vlrMaior100}. ####")
-print(f"#### C) Qual é o nome do produto mais barato: {prdMaisbarato}. ####")
+print(f"#### A) Qual é o total gasto na compra: {vlrCompra:.2f}.")
+print(f"#### B) Quantos produtos custam mais de R$ 100,00: {vlrMaior100}.")
+print(f"#### C) Qual é o nome do produto mais barato: {nmProdutoMaisBarato}.")
