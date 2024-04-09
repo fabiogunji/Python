003e9bd757f9b35cfbee7e16dbd18d691c7ad756
Fabio Jorge Gunji <48600898+fabiogunji@users.noreply.github.com>
2024-04-09 00:45:42 -0300

    Update desafio6.py

--- Atividade Aula 05 - While/desafio6.py
+++ Atividade Aula 05 - While/desafio6.py
@@ -9,38 +9,37 @@
     # C) Qual é o nome do produto mais barato.
     
 
-vlrMaior100 = qtdProduto = totalGasto = contador = prdMaiort100 =  0
-vlrCompra =  vlrMenorPreco = 0.0    
-nmProdutoMaisBarato = " "
-dictProduto = {}
-
-def validaPreco(produto, preco, vlrMenorPreco):        
-    if (preco < vlrMenorPreco): 
-        dictProduto.update({produto:preco})       
-        
-        
+vlrMaior100 = qtdProduto = contador = 0
+vlrCompra =  vlwProdutoMaisBarato = 0.0  
+prdMaiort100 = 100.0  
+nmProdutoMaisBarato = " "               
 
 while True:    
-    produto = (input("Digite o nome do produto: ")).upper()
+    produto = (input("Digite o nome do produto: ")).upper()    
     preco = float(input("Digite o preço desse produto: "))   
       
-    nmProdutoMaisBarato = produto
-    qtdProduto += 1        
+    qtdProduto += 1     
+    contador += 1   
         
-    if preco > 100.0:
+    if preco > prdMaiort100:
         vlrMaior100 += 1  
         vlrCompra += preco  
     else:
         vlrCompra += preco        
-               
-    validaPreco(produto, preco, vlrMenorPreco)
-
+    
+    if contador == 1:
+        nmProdutoMaisBarato = produto
+        vlwProdutoMaisBarato = preco
+    elif preco < vlwProdutoMaisBarato:        
+        nmProdutoMaisBarato = produto
+        vlwProdutoMaisBarato = preco      
+    
     continuar = input("Deseja continuar? [S/N]: ").upper()
     
     if continuar == "N":        
         break
 
         
-print(f"#### A) Qual é o total gasto na compra: {vlrCompra:.2f}.")
+print(f"#### A) Qual é o total gasto na compra: R$ {vlrCompra:.2f}.")
 print(f"#### B) Quantos produtos custam mais de R$ 100,00: {vlrMaior100}.")
-print(f"#### C) Qual é o nome do produto mais barato: {nmProdutoMaisBarato}.")
+print(f"#### C) Qual é o nome do produto mais barato: {nmProdutoMaisBarato} com o valor de R$ {vlwProdutoMaisBarato:.2F}.")
