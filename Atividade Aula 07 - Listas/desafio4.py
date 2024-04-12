1368ee204dcd355cd2bc923d805826b20a64153a
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-12 02:00:11 -0300

    update

--- Atividade Aula 07 - Listas/desafio4.py
+++ Atividade Aula 07 - Listas/desafio4.py
@@ -5,3 +5,29 @@
 # Depois disso, crie duas listas extras que vão conter apenas os valores pares e os valores impares digitados, respectivamente.
 
 # Ao final, mostre o conteúdo das três listas geradas.
+
+lstValores = []
+lstValoresPares = []
+lstValoresImpares = []
+
+contador = 0
+
+while True:    
+    lstValores.append(int(input(f"Entre com o {contador}º número inteiro: ")))
+    contador += 1
+    
+    if input("Deseja continuar? [S/N]: ").upper() == "S":
+        continue
+    else:
+        break
+    
+for i in lstValores:
+    if i % 2 == 0:
+        lstValoresPares.append(i)
+    else:
+        lstValoresImpares.append(i)
+        
+    
+print(f"A lista original é {lstValores} e a lista de números pares é {lstValoresPares} e a lista de números ímpares é {lstValoresImpares}") 
+        
+    
--- Atividade Aula 07 - Listas/desafio5.py
+++ Atividade Aula 07 - Listas/desafio5.py
@@ -5,3 +5,31 @@
 # A) Quantas pessoas foram cadastradas.
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Um listagem com as pessoas mais leves
+
+lstNomePeso = []
+tpPessoas = ()
+contador = 1
+
+while True:    
+    nome = input(f"Entre com o {contador}º nome: ").upper()
+    peso = int(input(f"Entre com o {contador}º peso: "))
+    
+    tpNomePeso = (nome,peso)
+    lstNomePeso.append(tpNomePeso)
+    
+    contador += 1
+  
+    if input("Deseja continuar? [S/N]: ").upper() == "S":
+        continue
+    else:
+        break
+    
+tpPessoas = tuple(lstNomePeso)
+   
+
+# print(len(lstNomePeso))
+
+print(f"A) Quantas pessoas foram cadastradas é {len(lstNomePeso)}.")
+print(f"B) Uma listagem com as pessoas mais pesadas {sorted(tpPessoas)}")
+print(f"C) Um listagem com as pessoas mais leves")
+
