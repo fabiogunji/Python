18335378acbb613d0faf51e59f71ea6b41253746
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-13 07:33:51 -0300

    update Teste

---
+++ Testes/cliente.py
@@ -0,0 +1,19 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+cliente.connect(("localhost",8080))
+
+print("Digite SAIR para terminar o chat.")
+
+while True:
+    cliente.send(input("Mensagem: ").encode("UTF-8"))
+    
+    msg = cliente.recv(1024).decode("UTF-8")
+    
+    if msg == "SAIR":
+        break
+    else:
+        print(msg)
+        
+cliente.close()
---
+++ Testes/servidor.py
@@ -0,0 +1,25 @@
+import socket
+
+servidor = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+servidor.bind(("localhost",8080))
+
+servidor.listen()
+
+cliente, end = servidor.accept()
+
+while True:
+    msg =cliente.recv(1024).decode("UTF-8")
+    
+    if msg == "SAIR":
+        break
+    else:
+        print(msg)
+    
+    cliente.send(input("Mensagem: ").encode("UTF-8"))        
+    
+cliente.close()
+servidor.close()
+
+
+    
