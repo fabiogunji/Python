ffd062bffdfb2bd63ae1c1677372ea5109a0f30c
Fabio Jorge Gunji <fabio.gunji@gmail.com>
2024-04-23 16:34:35 -0300

    Update jogo.py

--- PyGame/jogo.py
+++ PyGame/jogo.py
@@ -8,10 +8,16 @@ altura = 480
 largura = 640
 
 # Definindo posições e tamanho de objetos
-pos_y = altura / 2
-pos_x = largura / 2
+pos_y_retangulo = altura / 2
+pos_x_retangulo = largura / 2
 largura_retangulo = 30
 altura_retangulo = 40
+
+# Definindo posição e tamanho do circulo
+pos_y_circulo = 40
+pos_x_circulo = 20
+raio_circulo = 10
+
 
 # Criando a janela do game
 tela = pygame.display.set_mode((largura,altura))
@@ -20,14 +26,33 @@ tela = pygame.display.set_mode((largura,altura))
 pygame.display.set_caption("Criando Jogos")
 
 while True:
+    tela.fill((0,0,0))
+    
     # Colocando os eventos no pygame
     for event in pygame.event.get():
         # Evento sair
         if event.type == QUIT:
             pygame.quit()
             exit()
+            
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                pos_y_retangulo -= 10
+            
+            if event.key == K_DOWN:
+                pos_y_retangulo += 10
+            
+            if event.key == K_LEFT:
+                pos_x_retangulo -= 10
+            
+            if event.key == K_RIGHT:
+                pos_x_retangulo += 10
+            
+        
     
-    pygame.draw.rect(tela, (0,0,255), (pos_x,pos_y,largura_retangulo, altura_retangulo))
+    pygame.draw.rect(tela, (0,0,255), (pos_x_retangulo,pos_y_retangulo,largura_retangulo, altura_retangulo))
+    
+    pygame.draw.circle(tela, (255,0,255), (pos_x_circulo,pos_y_circulo),raio_circulo)
 
     # Atualizar o jogo em toda a interação
     pygame.display.update()
